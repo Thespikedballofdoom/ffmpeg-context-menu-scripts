7dabd6f83acfc251be7b8c6fa0a6ab235bc79c97
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2021-09-24 06:03:22 -0400

    Add files via upload

---
+++ compress.bat
@@ -0,0 +1,8 @@
+set i=%~n1
+set x=%~x1
+set o=%i%.mp4
+if exist %o% (
+  set o=%i%-h264crf25.mp4
+)
+ffmpeg -i "%i%%x%" -vcodec libx264 -acodec aac -movflags +faststart -crf 25 "%o%"
+exit
---
+++ playableconvert.bat
@@ -0,0 +1,8 @@
+set i=%~n1
+set x=%~x1
+set o=%i%.mp4
+if exist %o% (
+  set o=%i%-h264_yuv240p.mp4
+)
+ffmpeg -i "%i%%x%" -vcodec libx264 -preset veryslow -acodec aac -movflags +faststart -crf 0 -qp 0 -vf format=yuv420p "%o%"
+exit
