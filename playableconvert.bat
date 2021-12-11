6c45677674104a11f795243cb6d8a89b7f0ee3be
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2021-12-11 09:41:42 -0500

    Delete playableconvert.bat

--- playableconvert.bat
+++
@@ -1,8 +0,0 @@
-set i=%~n1
-set x=%~x1
-set o=%i%.mp4
-if exist %o% (
-  set o=%i%-h264_yuv240p.mp4
-)
-ffmpeg -i "%i%%x%" -vcodec libx264 -preset veryslow -acodec aac -movflags +faststart -crf 1 -vf format=yuv420p "%o%"
-exit
