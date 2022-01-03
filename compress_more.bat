76f56f1ac099d9d7a3655030c591b66d61593764
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2022-01-03 14:06:09 -0500

    Delete compress_more.bat

--- compress_more.bat
+++
@@ -1,8 +0,0 @@
-set i=%~n1
-set x=%~x1
-set o=%i%.mp4
-if exist %o% (
-  set o=%i%-h264crf33.mp4
-)
-ffmpeg -i "%i%%x%" -vcodec libx264 -acodec aac -preset slow -pix_fmt yuv420p -acodec aac -movflags +faststart "%o%"
-exit
