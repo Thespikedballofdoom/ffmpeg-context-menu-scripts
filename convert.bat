c49b4e4cebd50aca07dd6fcaf3c81eee9d2a11e6
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2022-01-03 14:06:32 -0500

    Delete convert.bat

--- convert.bat
+++
@@ -1,8 +0,0 @@
-set i=%~n1
-set x=%~x1
-set o=%i%.mp4
-if exist %o% (
-  set o=%i%-h264crf0.mp4
-)
-ffmpeg -i "%i%%x%" -vcodec libx264 -preset veryslow -strict -2 -acodec aac -crf 0 -qp 0 -movflags +faststart "%o%"
-exit
